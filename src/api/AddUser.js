18eb56ad23f9948bb695fe3d4b275d2109b463b7
yuki sato <yuki3100705@gmail.com>
2020-06-24 18:25:09 +0900

    gitgnore

--- src/api/AddUser.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const url_users = 'http://localhost:3001/users';
-
-export default function AddUser(props) {
-  const user = { name : props.regname, address : props.regaddress };
-  axios.post(url_users, user);
-}
--- src/api/DeleteUser.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const url_users = 'http://localhost:3001/users';
-
-export default function DeleteUser(props) {
-  axios.delete(url_users + '/' + props.id);
-}
